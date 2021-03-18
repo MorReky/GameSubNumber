478a360e4012c2d5a6e72c0b72a4f6cb2e5cd9b1
MorReky <mormemir@yandex.ru>
2021-03-18 08:31:57 +0000

    Реализована множественная регистрация пользователей

---
+++ Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameSubNumber.Models
+{
+    /// <summary>
+    /// Модель Игрока
+    /// </summary>
+   internal class User
+    {
+        //Никнейм игрока
+        public string UserName;
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,37 @@
-﻿using System;
+﻿using GameSubNumber.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Documents;
 
 namespace GameSubNumber
 {
     class Program
     {
+        private static int Players=1;
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите количество игроков");
+            Players = Convert.ToInt32(Console.ReadLine());
+            IList<User> UsersList = new List<User>();
+            for (int i = 0; i < Players; i++)
+            {
+                Console.WriteLine("Введите ник для 'Игрок{0}'", i+1);
+                User user = new User
+                {
+                    UserName = Console.ReadLine()
+                };
+                UsersList.Add(user);
+            }
+            Console.WriteLine("Последовательность:");
+            foreach(User obj in UsersList)
+            {
+                Console.WriteLine(obj.UserName);
+            }
+            Console.ReadLine();
         }
     }
 }
+
